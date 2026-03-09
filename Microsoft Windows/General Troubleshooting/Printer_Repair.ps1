18ce11dc9e02e8c30dd50a8cb42ac500226e42d5
EpiicOtiis <201993436+EpiicOtiis@users.noreply.github.com>
2026-03-09 13:23:07 -0400

    Update Printer_Repair.ps1

--- Microsoft Windows/General Troubleshooting/Printer_Repair.ps1
+++ Microsoft Windows/General Troubleshooting/Printer_Repair.ps1
@@ -4,6 +4,15 @@
 # This script provides a menu for printer cleanup and listing functionalities.
 # Copyright: Aaron Pleus/Integris IT - Do not share or distribute.
 
+function RestartSpooler {
+    Write-Host "Stopping Print Spooler service..."
+    Get-Service *spool* | Stop-Service -Force -Verbose
+    Start-Sleep -Seconds 5
+
+    Write-Host "Starting Print Spooler service..."
+    Get-Service Spooler | Start-Service -Verbose
+}
+
 function CleanSpooler {
     Write-Host "Stopping Print Spooler service..."
     Get-Service *spool* | Stop-Service -Force -Verbose
@@ -30,21 +39,23 @@ function ExportPrinters {
 
 do {
     Write-Host "`nMenu:"
-    Write-Host "1. Clean up Print Spooler Queue"
-    Write-Host "2. List Installed Printers to Screen"
-    Write-Host "3. Export Installed Printers to CSV"
-    Write-Host "4. Exit"
-    $choice = Read-Host "Enter your choice (1-4)"
+    Write-Host "1. Restart Print Spooler Service"
+    Write-Host "2. Clean up Print Spooler Queue"
+    Write-Host "3. List Installed Printers to Screen"
+    Write-Host "4. Export Installed Printers to CSV"
+    Write-Host "5. Exit"
+    $choice = Read-Host "Enter your choice (1-5)"
 
     switch ($choice) {
-        1 { CleanSpooler }
-        2 { ListPrinters }
-        3 { ExportPrinters }
-        4 { break }
+        1 { RestartSpooler }
+        2 { CleanSpooler }
+        3 { ListPrinters }
+        4 { ExportPrinters }
+        5 { break }
         default { Write-Host "Invalid choice. Please try again." }
     }
 
-    if ($choice -ne 4) {
+    if ($choice -ne 5) {
         $again = Read-Host "Do you want to perform another action? (y/n)"
         if ($again -ne 'y') { break }
     }
